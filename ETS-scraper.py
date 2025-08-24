bbc17af285c529da27a11de20c5a0c9b2db860b9
Koay <koayyeeshuen@gmail.com>
2025-08-24 17:22:50 +0800

    Remove bin location

--- ETS-scraper.py
+++ ETS-scraper.py
@@ -83,9 +83,6 @@ def run_selenium(context_data, stop_event):
     options.add_argument('--disable-extensions')
     options.add_argument('--disable-infobars')
     options.add_argument('--window-size=1920,1080')
-    
-    # Explicitly set Chrome binary location
-    options.binary_location = '/usr/bin/google-chrome'
 
     service = Service(executable_path='/usr/bin/chromedriver')
     driver = webdriver.Chrome(
