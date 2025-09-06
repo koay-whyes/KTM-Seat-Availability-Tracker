92c8bd91d3309bc84e350fb0525444708a531caa
Koay <koayyeeshuen@gmail.com>
2025-09-06 16:18:23 +0800

    Debug run_selenium

--- ETS-scraper.py
+++ ETS-scraper.py
@@ -137,10 +137,11 @@ def run_selenium(context_data, stop_event):
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--headless=new')  # New headless mode
     options.add_argument('--disable-gpu')
-    options.add_argument('--remote-debugging-port=9222')
-    options.add_argument('--disable-extensions')
-    options.add_argument('--disable-infobars')
+    # options.add_argument('--remote-debugging-port=9222')
+    # options.add_argument('--disable-extensions')
+    # options.add_argument('--disable-infobars')
     options.add_argument('--window-size=1920,1080')
+    options.add_argument('--no-zygote')
 
     driver = webdriver.Chrome(
         service=Service(ChromeDriverManager().install()),
@@ -519,7 +520,7 @@ async def post_init(application: Application):
     ])
     await application.bot.set_chat_menu_button(menu_button=MenuButtonCommands())
 
-def error_handler(update: object, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def error_handler(update: object, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Log errors caused by Updates."""
     error = context.error
     if isinstance(error, Conflict):
